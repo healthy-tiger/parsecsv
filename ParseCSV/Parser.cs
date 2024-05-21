dce89adc152c3aea8b143d615f077d74c6cd25a0
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2024-05-21 11:20:22 +0900

    プロジェクト ファイルを追加します。

---
+++ ParseCSV/Parser.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HealthyTiger.CSVParser
+{
+    public class Parser
+    {
+        public static async Task<List<string>[]> ParseStreamAsync(Stream stream, char sep, CancellationToken token)
+        {
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                List<string> items = new List<string>();
+                List<List<string>> lines = new List<List<string>>();
+                bool inQuote = false;
+                char[] sepchars = new char[] { sep };
+                string sepstr = new string(sepchars);
+
+                string line = null;
+                while (!token.IsCancellationRequested)
+                {
+                    line = await sr.ReadLineAsync();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    string[] cols = line.Split(sepchars, StringSplitOptions.None);
+                    bool isLeftMostColumn = true;
+                    foreach (string col in cols)
+                    {
+                        if (inQuote)
+                        {
+                            var tmp = items[items.Count - 1];
+                            if (!isLeftMostColumn)
+                            {
+                                tmp += sepstr;
+                            }
+                            var t = col.TrimEnd();
+                            if (t.EndsWith("\""))
+                            {
+                                if (items.Count == 0)
+                                {
+                                    throw new FormatException("unmatched double quotes");
+                                }
+                                tmp += t.Substring(0, t.Length - 1);
+                                inQuote = false;
+                            }
+                            else
+                            {
+                                tmp += col;
+                            }
+                            items[items.Count - 1] = tmp;
+                        }
+                        else
+                        {
+                            var t = col.TrimStart();
+                            if (t.StartsWith("\""))
+                            {
+                                var trim = t.TrimEnd();
+                                if (trim.EndsWith("\""))
+                                {
+                                    items.Add(trim.Substring(1, trim.Length - 2));
+                                }
+                                else
+                                {
+                                    items.Add(t.Substring(1));
+                                    inQuote = true;
+                                }
+                            }
+                            else
+                            {
+                                items.Add(col.Trim());
+                            }
+                        }
+                        isLeftMostColumn = false;
+                    }
+                    if (inQuote)
+                    {
+                        items[items.Count - 1] += "\n";
+                    }
+                    else
+                    {
+                        lines.Add(items);
+                        items = new List<string>();
+                    }
+                }
+                return lines.ToArray();
+            }
+        }
+    }
+}
---
+++ ParseCSVTest/ParseTest.cs
@@ -0,0 +1,76 @@
+using System.Text;
+using HealthyTiger.CSVParser;
+
+namespace ParseCSVTest
+{
+    [TestClass]
+    public class ParseTest
+    {
+        [TestMethod]
+        public async Task SingleLine()
+        {
+            string[] src = new string[] {
+                "1,2,3,10,20,30",
+                "1,2,\"(x,y)\",3,4,\"[a,b]\"",
+                "1,2,\"(x,y)\",3,4,\"[a,b]\",5,6",
+                "\"(x,y)\",3,4,\"[a,b]\",5,6",
+                "\"1\",2,3,4",
+                "1,\"2\",3,4",
+                "1,2,3,\"4\"",
+                "1,2,\"\",4",
+            };
+
+            string[] expected = [
+                "1,2,3,10,20,30",
+                "1,2,(x,y),3,4,[a,b]",
+                "1,2,(x,y),3,4,[a,b],5,6",
+                "(x,y),3,4,[a,b],5,6",
+                "1,2,3,4",
+                "1,2,3,4",
+                "1,2,3,4",
+                "1,2,,4",
+            ];
+
+            for(int i = 0; i < src.Length; i++)
+            {
+                string s = src[i];
+                string e = expected[i];
+                MemoryStream ms = new(Encoding.UTF8.GetBytes(s));
+                List<string>[] ans = await Parser.ParseStreamAsync(ms, ',', CancellationToken.None);
+                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
+                Assert.AreEqual(e, a, false);
+            }
+
+        }
+
+        [TestMethod]
+        public async Task MultiLine()
+        {
+            string[] src =
+            {
+                "1,2,\"(x\r\n,y)\",3,4,\"[\r\na,b]\"",
+                "1,2,\"(x\r\n,y)\",3,4,\"[a,b]\r\n\",5,6",
+                "1,2,\"(x\r\n,y)\",3,4,\"[a,\r\nb]\",5,6",
+                "\"(\r\nx,y)\",3,4,\"[a,b]\",5,6",
+                "\"(\r\nx,y)\",\"3\",4,\"[a,b]\",5,6",
+            };
+            string[] expected =
+            {
+                "1,2,(x\n,y),3,4,[\na,b]",
+                "1,2,(x\n,y),3,4,[a,b]\n,5,6",
+                "1,2,(x\n,y),3,4,[a,\nb],5,6",
+                "(\nx,y),3,4,[a,b],5,6",
+                "(\nx,y),3,4,[a,b],5,6",
+            };
+            for (int i = 0; i < src.Length; i++)
+            {
+                string s = src[i];
+                string e = expected[i];
+                MemoryStream ms = new(Encoding.UTF8.GetBytes(s));
+                List<string>[] ans = await Parser.ParseStreamAsync(ms, ',', CancellationToken.None);
+                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
+                Assert.AreEqual(e, a, false);
+            }
+        }
+    }
+}
