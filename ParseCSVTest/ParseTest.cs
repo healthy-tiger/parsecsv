56218b335ba9ea9972a28285c1d633cfb56a308e
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2024-06-23 15:13:46 +0900

    RFC4180のテストを一部追加

--- ParseCSVTest/ParseTest.cs
+++ ParseCSVTest/ParseTest.cs
@@ -1,3 +1,4 @@
+using System.IO.Pipes;
 using System.Text;
 using HealthyTiger.CSVParser;
 
@@ -6,6 +7,34 @@ namespace ParseCSVTest
     [TestClass]
     public class ParseTest
     {
+        public static bool CsvEquals(List<List<string>> a, List<List<string>> b)
+        {
+            if (a.Count != b.Count) return false;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                var ai = a[i];
+                var bi = b[i];
+
+                if (ai.Count != bi.Count)
+                {
+                    return false;
+                }
+
+                for (int j = 0; j < ai.Count; j++)
+                {
+                    var astr = ai[j];
+                    var bstr = bi[j];
+
+                    if (!astr.Equals(bstr))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
         [TestMethod]
         public async Task SingleLineAsync()
         {
@@ -20,25 +49,24 @@ namespace ParseCSVTest
                 "1,2,\"\",4",
             };
 
-            string[] expected = [
-                "1,2,3,10,20,30",
-                "1,2,(x,y),3,4,[a,b]",
-                "1,2,(x,y),3,4,[a,b],5,6",
-                "(x,y),3,4,[a,b],5,6",
-                "1,2,3,4",
-                "1,2,3,4",
-                "1,2,3,4",
-                "1,2,,4",
+            List<List<string>>[] expected = [
+                [["1","2","3","10","20","30"]],
+                [["1","2","(x,y)","3","4","[a,b]"]],
+                [["1","2","(x,y)","3","4","[a,b]","5","6"]],
+                [["(x,y)","3","4","[a,b]","5","6"]],
+                [["1","2","3","4"]],
+                [["1","2","3","4"]],
+                [["1","2","3","4"]],
+                [["1","2","","4"]],
             ];
 
-            for(int i = 0; i < src.Length; i++)
+            for (int i = 0; i < src.Length; i++)
             {
                 string s = src[i];
-                string e = expected[i];
+                List<List<string>> e = expected[i];
                 MemoryStream ms = new(Encoding.UTF8.GetBytes(s));
                 List<List<string>> ans = await Parser.ParseAsync(ms, ',', CancellationToken.None);
-                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
-                Assert.AreEqual(e, a, false);
+                Assert.AreEqual(CsvEquals(e, ans), true);
             }
 
         }
@@ -46,31 +74,24 @@ namespace ParseCSVTest
         [TestMethod]
         public async Task MultiLineAsync()
         {
-            string[] src =
-            {
+            string src = string.Join("\r\n", [
                 "1,2,\"(x\r\n,y)\",3,4,\"[\r\na,b]\"",
                 "1,2,\"(x\r\n,y)\",3,4,\"[a,b]\r\n\",5,6",
                 "1,2,\"(x\r\n,y)\",3,4,\"[a,\r\nb]\",5,6",
                 "\"(\r\nx,y)\",3,4,\"[a,b]\",5,6",
                 "\"(\r\nx,y)\",\"3\",4,\"[a,b]\",5,6",
-            };
-            string[] expected =
-            {
-                "1,2,(x\n,y),3,4,[\na,b]",
-                "1,2,(x\n,y),3,4,[a,b]\n,5,6",
-                "1,2,(x\n,y),3,4,[a,\nb],5,6",
-                "(\nx,y),3,4,[a,b],5,6",
-                "(\nx,y),3,4,[a,b],5,6",
-            };
-            for (int i = 0; i < src.Length; i++)
-            {
-                string s = src[i];
-                string e = expected[i];
-                MemoryStream ms = new(Encoding.UTF8.GetBytes(s));
-                List<List<string>> ans = await Parser.ParseAsync(ms, ',', CancellationToken.None);
-                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
-                Assert.AreEqual(e, a, false);
-            }
+            ]);
+            List<List<string>> expected =
+            [
+                ["1","2","(x\n,y)","3","4","[\na,b]"],
+                ["1","2","(x\n,y)","3","4","[a,b]\n","5","6"],
+                ["1","2","(x\n,y)","3","4","[a,\nb]","5","6"],
+                ["(\nx,y)","3","4","[a,b]","5","6"],
+                ["(\nx,y)","3","4","[a,b]","5","6"],
+            ];
+            MemoryStream ms = new(Encoding.UTF8.GetBytes(src));
+            List<List<string>> ans = await Parser.ParseAsync(ms, ',', CancellationToken.None);
+            Assert.AreEqual(CsvEquals(expected, ans), true);
         }
 
         [TestMethod]
@@ -87,57 +108,48 @@ namespace ParseCSVTest
                 "1,2,\"\",4",
             };
 
-            string[] expected = [
-                "1,2,3,10,20,30",
-                "1,2,(x,y),3,4,[a,b]",
-                "1,2,(x,y),3,4,[a,b],5,6",
-                "(x,y),3,4,[a,b],5,6",
-                "1,2,3,4",
-                "1,2,3,4",
-                "1,2,3,4",
-                "1,2,,4",
+            List<List<string>>[] expected = [
+                [["1","2","3","10","20","30"]],
+                [["1","2","(x,y)","3","4","[a,b]"]],
+                [["1","2","(x,y)","3","4","[a,b]","5","6"]],
+                [["(x,y)","3","4","[a,b]","5","6"]],
+                [["1","2","3","4"]],
+                [["1","2","3","4"]],
+                [["1","2","3","4"]],
+                [["1","2","","4"]],
             ];
 
             for (int i = 0; i < src.Length; i++)
             {
                 string s = src[i];
-                string e = expected[i];
-                MemoryStream ms = new(Encoding.UTF8.GetBytes(s));
-                List<List<string>> ans = Parser.Parse(ms, ',');
-                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
-                Assert.AreEqual(e, a, false);
+                List<List<string>> e = expected[i];
+                List<List<string>> ans = RFC4180.ParseString(s);
+                Assert.AreEqual(CsvEquals(e, ans), true);
             }
-
         }
 
         [TestMethod]
         public void MultiLine()
         {
-            string[] src =
-            {
+            string src = string.Join("\r\n", [
                 "1,2,\"(x\r\n,y)\",3,4,\"[\r\na,b]\"",
                 "1,2,\"(x\r\n,y)\",3,4,\"[a,b]\r\n\",5,6",
                 "1,2,\"(x\r\n,y)\",3,4,\"[a,\r\nb]\",5,6",
                 "\"(\r\nx,y)\",3,4,\"[a,b]\",5,6",
                 "\"(\r\nx,y)\",\"3\",4,\"[a,b]\",5,6",
-            };
-            string[] expected =
-            {
-                "1,2,(x\n,y),3,4,[\na,b]",
-                "1,2,(x\n,y),3,4,[a,b]\n,5,6",
-                "1,2,(x\n,y),3,4,[a,\nb],5,6",
-                "(\nx,y),3,4,[a,b],5,6",
-                "(\nx,y),3,4,[a,b],5,6",
-            };
-            for (int i = 0; i < src.Length; i++)
-            {
-                string s = src[i];
-                string e = expected[i];
-                MemoryStream ms = new(Encoding.UTF8.GetBytes(s));
-                List<List<string>> ans = Parser.Parse(ms, ',');
-                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
-                Assert.AreEqual(e, a, false);
-            }
+            ]);
+            // TODO ññîˆÇ…â¸çsÇ™Ç†ÇÈèÍçáÇ∆Ç»Ç¢èÍçá
+            List<List<string>> expected =
+            [
+                ["1","2","(x\n,y)","3","4","[\na,b]"],
+                ["1","2","(x\n,y)","3","4","[a,b]\n","5","6"],
+                ["1","2","(x\n,y)","3","4","[a,\nb]","5","6"],
+                ["(\nx,y)","3","4","[a,b]","5","6"],
+                ["(\nx,y)","3","4","[a,b]","5","6"],
+            ];
+            MemoryStream ms = new(Encoding.UTF8.GetBytes(src));
+            List<List<string>> ans = Parser.Parse(ms, ',');
+            Assert.AreEqual(CsvEquals(expected, ans), true);
         }
 
         [TestMethod]
@@ -154,55 +166,80 @@ namespace ParseCSVTest
                 "1,2,\"\",4",
             };
 
-            string[] expected = [
-                "1,2,3,10,20,30",
-                "1,2,(x,y),3,4,[a,b]",
-                "1,2,(x,y),3,4,[a,b],5,6",
-                "(x,y),3,4,[a,b],5,6",
-                "1,2,3,4",
-                "1,2,3,4",
-                "1,2,3,4",
-                "1,2,,4",
+            List<List<string>>[] expected = [
+                [["1","2","3","10","20","30"]],
+                [["1","2","(x,y)","3","4","[a,b]"]],
+                [["1","2","(x,y)","3","4","[a,b]","5","6"]],
+                [["(x,y)","3","4","[a,b]","5","6"]],
+                [["1","2","3","4"]],
+                [["1","2","3","4"]],
+                [["1","2","3","4"]],
+                [["1","2","","4"]],
             ];
 
             for (int i = 0; i < src.Length; i++)
             {
                 string s = src[i];
-                string e = expected[i];
+                List<List<string>> e = expected[i];
                 List<List<string>> ans = Parser.Parse(s, ',');
-                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
-                Assert.AreEqual(e, a, false);
+                Assert.AreEqual(CsvEquals(e, ans), true);
             }
-
         }
 
         [TestMethod]
         public void MultiLineString()
         {
-            string[] src =
-            {
+            string src = string.Join("\r\n", [
                 "1,2,\"(x\r\n,y)\",3,4,\"[\r\na,b]\"",
                 "1,2,\"(x\r\n,y)\",3,4,\"[a,b]\r\n\",5,6",
                 "1,2,\"(x\r\n,y)\",3,4,\"[a,\r\nb]\",5,6",
                 "\"(\r\nx,y)\",3,4,\"[a,b]\",5,6",
                 "\"(\r\nx,y)\",\"3\",4,\"[a,b]\",5,6",
-            };
-            string[] expected =
+            ]);
+            List<List<string>> expected =
+            [
+                ["1","2","(x\n,y)","3","4","[\na,b]"],
+                ["1","2","(x\n,y)","3","4","[a,b]\n","5","6"],
+                ["1","2","(x\n,y)","3","4","[a,\nb]","5","6"],
+                ["(\nx,y)","3","4","[a,b]","5","6"],
+                ["(\nx,y)","3","4","[a,b]","5","6"],
+            ];
+            List<List<string>> ans = Parser.Parse(src, ',');
+            Assert.AreEqual(CsvEquals(expected, ans), true);
+        }
+
+        [TestMethod]
+        public async Task CancelStreamAsync()
+        {
+            using AnonymousPipeServerStream server = new(direction: PipeDirection.Out);
+            using AnonymousPipeClientStream client = new(PipeDirection.In, server.GetClientHandleAsString());
+            CancellationTokenSource cts = new();
+
+            Task writer = Task.Run(() =>
             {
-                "1,2,(x\n,y),3,4,[\na,b]",
-                "1,2,(x\n,y),3,4,[a,b]\n,5,6",
-                "1,2,(x\n,y),3,4,[a,\nb],5,6",
-                "(\nx,y),3,4,[a,b],5,6",
-                "(\nx,y),3,4,[a,b],5,6",
-            };
-            for (int i = 0; i < src.Length; i++)
+                Thread.Sleep(100);
+                using StreamWriter sw = new(server);
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine($"{i},1,2,3");
+                }
+                cts.Cancel();
+            });
+
+            bool isCanceled = false;
+            List<List<string>>? lines = null;
+            try
             {
-                string s = src[i];
-                string e = expected[i];
-                List<List<string>> ans = Parser.Parse(s, ',');
-                string a = string.Join("\n", ans.Select(x => string.Join(",", x)));
-                Assert.AreEqual(e, a, false);
+                lines = await Parser.ParseAsync(client, ',', cts.Token);
             }
+            catch (OperationCanceledException)
+            {
+                isCanceled = true;
+            }
+
+            await writer;
+
+            Assert.AreEqual(true, isCanceled);
         }
     }
 }
